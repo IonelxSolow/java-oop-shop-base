452a33b6f048ecee02aff6d725be608a5a6d6e4b
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-06-11 21:58:28 +0200

    Update Prodotto instantiation in Main to use BigDecimal for price, enhancing precision

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -23,13 +23,13 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
-        Prodotto prodotto = new Prodotto("Banana", "Gialla e buona", new BigDecimal(2), new BigDecimal(0.22));
+        Prodotto prodotto = new Prodotto("Banana", "Gialla e buona", new BigDecimal(2.5), new BigDecimal(0.22));
 
         System.out.println("Codice: " + prodotto.codice);
         System.out.println("Nome: " + prodotto.nome);
         System.out.println("Descrizione: " + prodotto.descrizione);
         System.out.println("Prezzo: " + prodotto.prezzo);
-        System.out.println("IVA: " + prodotto.iva);
+        System.out.println("IVA: " + prodotto.iva.doubleValue());
         System.out.println(prodotto.getPrezzoBase());
         System.out.println(prodotto.getPrezzoIvato());
         System.out.println(prodotto.getNomeEsteso());
