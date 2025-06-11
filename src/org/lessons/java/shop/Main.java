e63e605a5bde14848d56dcb72b167dc435de787b
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-06-11 21:49:20 +0200

    Refactor Main and Prodotto classes to use BigDecimal for price and VAT, enhancing precision and clarity

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,9 +1,9 @@
 package org.lessons.java.shop;
 
-public class Main {
+/* public class Main {
     public static void main(String[] args) {
 
-        Prodotto prodotto = new Prodotto("Banana", "Gialla e buona", 2.5, 22);
+        Prodotto prodotto = new Prodotto("Banana", "Gialla e buona", 2, 22);
 
         System.out.println("Codice: " + prodotto.codice);
         System.out.println("Nome: " + prodotto.nome);
@@ -15,5 +15,24 @@ public class Main {
         prodotto.prezzoBase();
         prodotto.prezzoBaseIva();
         prodotto.nomeEsteso();
+    }
+} */
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Prodotto prodotto = new Prodotto("Banana", "Gialla e buona", new BigDecimal(2), new BigDecimal(0.22));
+
+        System.out.println("Codice: " + prodotto.codice);
+        System.out.println("Nome: " + prodotto.nome);
+        System.out.println("Descrizione: " + prodotto.descrizione);
+        System.out.println("Prezzo: " + prodotto.prezzo);
+        System.out.println("IVA: " + prodotto.iva);
+        System.out.println(prodotto.getPrezzoBase());
+        System.out.println(prodotto.getPrezzoIvato());
+        System.out.println(prodotto.getNomeEsteso());
+
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,6 +1,7 @@
 package org.lessons.java.shop;
 
-import java.util.Random;
+/* import java.util.Random;
+
 
 public class Prodotto {
      int codice;
@@ -30,5 +31,46 @@ public class Prodotto {
         System.out.println("Il nome esteso codice-nome è: " + this.codice + "-" + this.nome);
     }
 
+
+} */
+
+import java.util.Random;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Prodotto {
+    public int codice; // default null
+    public String nome; //default null
+    public String descrizione; // default null
+    public BigDecimal prezzo; // default 0
+    public BigDecimal iva; // default 0
+
+    Prodotto(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva) {
+        Random rand = new Random();
+        this.codice = rand.nextInt(99999);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva; // 0.22
+    }
+
+    public BigDecimal getPrezzoBase() {
+        return this.prezzo;
+    }
+
+    public BigDecimal getPrezzoIvato() {
+        if(prezzo != null && iva != null){
+            // stiamo verificando che siano inizializzate le nostre variabili di istanza
+            return prezzo.add(prezzo.multiply(iva)).setScale(2, RoundingMode.DOWN);
+        } // non ci servirà eseguire la nostra operazione e ritorneremo null
+        return null;
+    }
+
+    public String getNomeEsteso() {
+        if(nome != null){
+             return codice + "-" + nome;
+        }
+        return null;
+    }
 
 }
